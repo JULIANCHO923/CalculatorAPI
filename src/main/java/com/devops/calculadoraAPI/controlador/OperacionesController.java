c41dd272d30e658a2726b0a6c07f73cfb4988229
JULIANCHO923 <julianhenao92@gmail.com>
2020-10-25 07:52:55 -0500

    Update OperacionesController.java

--- src/main/java/com/devops/calculadoraAPI/controlador/OperacionesController.java
+++ src/main/java/com/devops/calculadoraAPI/controlador/OperacionesController.java
@@ -19,7 +19,7 @@ public class OperacionesController {
 
     @RequestMapping(value="/suma/{n1}/{n2}",method = RequestMethod.GET)
     private ResponseEntity<Double> suma(@PathVariable("n1") double n1,@PathVariable("n2") double n2){
-        return ResponseEntity.status(200).body(calculadora.suma(n1,n2));
+        return ResponseEntity.status(200).body("<html><body><title>suma</title>La suma entre "+ n1 + "+" + n2 + " = " + calculadora.suma(n1,n2) + "</body></html>");
     }
 
     @RequestMapping(value="/resta/{n1}/{n2}",method = RequestMethod.GET)
