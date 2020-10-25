0d07fe3c7fec7f0708b965d2fa55c23ddd1b5f9f
JULIANCHO923 <julianhenao92@gmail.com>
2020-10-25 08:01:59 -0500

    Update OperacionesController.java

--- src/main/java/com/devops/calculadoraAPI/controlador/OperacionesController.java
+++ src/main/java/com/devops/calculadoraAPI/controlador/OperacionesController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.net.InetAddress;
 
 @RestController
@@ -19,22 +18,18 @@ public class OperacionesController {
 
     @RequestMapping(value="/suma/{n1}/{n2}",method = RequestMethod.GET)
     private ResponseEntity<Double> suma(@PathVariable("n1") double n1,@PathVariable("n2") double n2){
-        return ResponseEntity.status(200).body("<html><body><title>suma</title>La suma entre "+ n1 + "+" + n2 + " = " + calculadora.suma(n1,n2) + "</body></html>");
+        return ResponseEntity.status(200).body("<html><body><title>suma</title>La suma entre "+n1+"+"+n2+" = "+ calculadora.suma(n1,n2) +"</body></html>");
     }
-
     @RequestMapping(value="/resta/{n1}/{n2}",method = RequestMethod.GET)
     public ResponseEntity<Double> resta(@PathVariable("n1") double n1,@PathVariable("n2") double n2){
-        return ResponseEntity.status(200).body(calculadora.resta(n1,n2));
+        return ResponseEntity.status(200).body(("<html><body><title>resta</title>La resta entre "+ n1 + "-" + n2 + " = " + calculadora.resta(n1,n2) + "</body></html>"););
     }
-
     @RequestMapping(value="/multiplicacion/{n1}/{n2}",method = RequestMethod.GET)
     public ResponseEntity<Double> multiplicacion(@PathVariable("n1") double n1,@PathVariable("n2") double n2){
-        return ResponseEntity.status(200).body(calculadora.multiplicacion(n1,n2));
+        return ResponseEntity.status(200).body("<html><body><title>multiplicacion</title>La multiplicacion entre "+ n1 + "*" + n2 + " = " + calculadora.multiplicacion(n1,n2) + "</body></html>");
     }
-
     @RequestMapping(value="/division/{n1}/{n2}",method = RequestMethod.GET)
     public ResponseEntity<Double> division(@PathVariable("n1") double n1,@PathVariable("n2") double n2){
-        return ResponseEntity.status(200).body(calculadora.division(n1,n2));
+        return ResponseEntity.status(200).body("<html><body><title>division</title>La division entre "+ n1 + "/" + n2 + " = " + calculadora.division(n1,n2) + "</body></html>");    
     }
-
 }
